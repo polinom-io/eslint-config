e3a1b289488fa1ab2f3e295da79708666e94daf8
Mario Espinoza <mario@23people.io>
2019-11-10 12:57:40 -0300

    config lint staged

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 module.exports = {
-
-  globals: {
-    MyGlobal: true
+  env: {
+    commonjs: true,
+    es2020: true,
+    node: true
   },
-
+  extends: [
+    'standard'
+  ],
+  globals: {
+    MyGlobal: true,
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
   rules: {
-    "semi": [
-      "error",
-      "always"
+    semi: [
+      'error',
+      'always'
     ],
-    "space-before-function-paren": [
-      "error",
+    'space-before-function-paren': [
+      'error',
       {
-        "anonymous": "always",
-        "named": "never",
-        "asyncArrow": "always"
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
       }
     ]
   }
